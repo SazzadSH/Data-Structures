49a5583db4a16608e2a21e88aace21e184ceb1af
Sazzad Hossen <thesazzadsh@gmail.com>
2018-04-04 20:21:44 +0600

    Half done C++ Queue

--- C++/Queue/QueueBasedOnArray.cpp
+++ C++/Queue/QueueBasedOnArray.cpp
@@ -2,73 +2,32 @@
 
 using namespace std;
 
-#define queueSize 100
+#define queueSize 10;
 
 class Queue
 {
 private:
-    int ara[queueSize];
-    int front, rear, counter;
+    int queueArray[queueSize];
+    int front, rear;
 
 public:
-
     Queue()
     {
-        front = -1;
-        rear = -1;
+        front = 0;
+        rear = 0;
     }
-
     void Enqueue(int data)
     {
         if(this->isFull())
         {
-            cout << "Failed! Queue is Full." << endl;
+            cout << "Queue is Full!" << endl;
         }
         else
         {
-            if(rear < queueSize-1)
-            {
-                ara[++rear] = data;
-            }
-            else
+            if(rear == (queueSize-1))
             {
                 rear = -1;
-                this->Enqueue(data);
             }
-        }
-    }
-
-    int Dequeue()
-    {
-        if(isEmpty())
-        {
-            cout << "Failed! Queue is empty." << endl;
-        }
-        else
-        {
-            if(front < queueSize-1)
-            {
-                int data = ara[++front];
-                ara[front] = NULL;
-                return data;
-            }
-            else
-            {
-                front = -1;
-                return this->Dequeue();
-            }
-        }
-    }
-
-    bool isFull()
-    {
-        if(((rear == queueSize) && (front == 0)) || (rear == (front - 1)))
-        {
-            return 1;
-        }
-        else
-        {
-            return 0;
         }
     }
 
@@ -84,18 +43,20 @@ public:
         }
     }
 
+    bool isFull()
+    {
+        if(((rear == queueSize-1) && (front == -1)) || (rear == (front-1)))
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
 };
 
 int main()
 {
-    Queue test;
-    for(int i = 0; i < 102; i++)
-    {
-        test.Enqueue((i+3)*4);
-    }
 
-    for(int i = 0; i < 102; i++)
-    {
-        cout << test.Dequeue() << endl;
-    }
 }
