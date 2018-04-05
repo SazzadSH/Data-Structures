e3c5d60dac2ababf630f0c3337ef1db2489c2ac9
Sazzad Hossen <thesazzadsh@gmail.com>
2018-04-05 20:51:27 +0600

    Half done C++ Queue Refactor 3

--- C++/Queue/QueueBasedOnArray.cpp
+++ C++/Queue/QueueBasedOnArray.cpp
@@ -6,70 +6,14 @@ using namespace std;
 
 class Queue
 {
-private:
+public:
     int queueArray[queueSize];
     int front, rear;
 
-public:
     Queue()
     {
         front = 0;
         rear = 0;
-    }
-    void Enqueue(int data)
-    {
-        if(this->isFull())
-        {
-            cout << "Queue is Full!" << endl;
-        }
-        else
-        {
-            if(rear == queueSize)
-            {
-                rear = -1;
-
-                this->Enqueue(data);
-            }
-            else
-            {
-                if(rear == -1)
-                {
-                    rear = 0;
-                }
-
-                queueArray[rear] = data;
-                rear++;
-            }
-        }
-    }
-
-    int Dequeue()
-    {
-        if(isEmpty())
-        {
-            cout << "Queue is Empty!" << endl;
-        }
-        else
-        {
-            if(front == queueSize)
-            {
-                front = -1;
-
-                this->Dequeue();
-            }
-            else
-            {
-                if(front == -1)
-                {
-                    front = 0;
-                }
-
-                int data = queueArray[front];
-                queueArray[front] = NULL;
-                front++;
-                return data;
-            }
-        }
     }
 
     bool isEmpty()
@@ -99,15 +43,5 @@ public:
 
 int main()
 {
-    Queue test;
 
-    for(int i = 0; i < 10; i++)
-    {
-        test.Enqueue((i+3)*4);
-    }
-
-    for(int i = 0; i < 10; i++)
-    {
-       cout << test.Dequeue() << endl;
-    }
 }
